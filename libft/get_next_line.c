a2b61c35d99e0ec8c156dedcb26ba8ad94d0c10c
Stackspear Vivienne <svivienn@at-l3.21-school.ru>
2019-04-23 17:44:04 +0300

    переделал мейк либы

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libft.h"
 
 static t_list	*take_fd(const int fd, t_list **list)
 {
